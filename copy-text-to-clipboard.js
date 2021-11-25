6867eca56873ea39028dbc45f632be0fc12ad8e5
phucbm <hiphucbui@gmail.com>
2021-11-25 22:40:40 +0700

    :sparkles: Copy text to clipboard from given value

--- copy-text-to-clipboard.js
+++ copy-text-to-clipboard.js
@@ -1,9 +1,25 @@
 /**
- * Copy text to clipboard
+ * Copy text to clipboard from input field
  * @param $target
  */
 function copyTextToClipboard($target){
     $target.select();
     document.execCommand("copy");
     $target.blur();
+}
+
+
+/**
+ * Copy text to clipboard from given value
+ * @param val
+ */
+const copyValueToClipboard = (val) => {
+    const dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+
+    dummy.setAttribute("id", "dummy_id");
+    document.getElementById("dummy_id").value = val;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
 }
